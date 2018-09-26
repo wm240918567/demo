67a568adbed78f16b568c3084cf4fe4247212833
wm <240918567@qq.com>
2018-09-26 23:30:59 +0800

    swaagerAPI 及一些自己的理解

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,48 @@
 package com.example.demo;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.http.MediaType;
 
-@EnableReactiveMongoRepositories
-@SpringBootApplication
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 启动类
+ */
+@EnableReactiveMongoRepositories//开启流mongodb
+@SpringBootApplication(scanBasePackages = {"com.example.demo"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	/**
+	 * 使用注入bean方式集成json解析框架：fastjson
+	 * 可以避免单继承的缺点，配置spring-boot-starter-tomcat时使用启动类继承时冲突
+	 * @return HttpMessageConverters
+	 */
+	@Bean
+	public HttpMessageConverters fastJsonHttpMessageConverters() {
+		// 先定义一个fastJsonHttpMessageConverte转换消息的对象
+		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+		// 添加fastJson的配置信息，比如是否需要格式化返回的JSON数据
+		FastJsonConfig fastJsonConfig = new FastJsonConfig();
+		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+		// 解决fastJson中文乱码问题
+		List<MediaType> fastMediaTypes = new ArrayList<MediaType>();
+		fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+		fastConverter.setSupportedMediaTypes(fastMediaTypes);
+		// 在fastJsonHttpMessageConverter添加fastJson的配置信息
+		fastConverter.setFastJsonConfig(fastJsonConfig);
+		//返回一个消息转换器
+		return new HttpMessageConverters(fastConverter);
 	}
 }
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,13 +2,23 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.User;
 import com.example.demo.repository.UserRepository;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+/**
+ * webflux使用mvc方式控制器
+ */
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -16,16 +26,63 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/")
+    /**
+     * 流式返回所有数据
+     * @return Flux包装的user对象
+     */
+    @ApiOperation(value = "查找所有",notes = "查找所有")
+    @GetMapping(value = "/findAllStream",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<User> findAllStream(){
+        return userRepository.findAll().map(user->{
+            try {
+                log.info(user.toString());
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return user;
+        });
+    }
+
+    /**
+     * JSON直接返回所有
+     * APPLICATION_JSON_UTF8_VALUE == @ResponseBody
+     * @return Flux包装的user对象
+     */
+    @ApiOperation(value = "JSON直接返回所有",notes = "JSON直接返回所有")
+    @GetMapping(value = "/findAll",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Flux<User> findAll(){
         return userRepository.findAll();
     }
 
+    /**
+     * 查找所有返回带有http状态码
+     * @return
+     */
+    @ApiOperation(value = "查找所有返回带有http状态码",notes = "查找所有返回带有http状态码")
+    @GetMapping(value = "/findAllResp")
+    public Mono<ResponseEntity<Flux<User>>> findAllResp(){
+        return Mono.just(new ResponseEntity<Flux<User>>(userRepository.findAll(),HttpStatus.OK));
+    }
+
+    /**
+     * 保存一个对象
+      * @param user 规定JSON格式
+     * @return
+     */
+    @ApiOperation(value = "保存一个对象",notes = "保存一个对象")
     @PostMapping("/")
     public Mono<User> save(@RequestBody User user){
         return userRepository.save(user);
     }
 
+    /**
+     * 根据主键更新
+     * @param id
+     * @param user
+     * @return
+     */
+    @ApiOperation(value = "根据主键更新",notes = "根据主键更新")
     @PutMapping("/{id}")
     public Mono<ResponseEntity<User>> update(@PathVariable("id")String id, @RequestBody User user){
         return userRepository.findById(id).flatMap(u->{
@@ -36,6 +93,12 @@ public class UserController {
                 .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    /**
+     * 根据ID删除
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "根据ID删除",notes = "根据ID删除")
     @DeleteMapping("/{id}")
     public Mono<ResponseEntity<Void>> deleteById(@PathVariable("id")String id){
         return userRepository.findById(id).flatMap(
